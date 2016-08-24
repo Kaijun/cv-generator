86301b062945f96bfca395cc44cb9be34206f7b8
Kaijun Chen <Demonbane.cn@gmail.com>
2016-08-24 19:02:51 +0200

    init

---
+++ data/resume.js
@@ -0,0 +1,90 @@
+module.exports = {
+  name: "陈凯俊",
+  intro: "I'm Kaijun, i want to use Markdown here!",
+  contact: [
+    {
+      title: "Mail",
+      content: "kaijun.cn@gmail.com"
+    },
+    {
+      title: "Web",
+      content: "http://kaijun.rocks",
+      url: "http://kaijun.rocks"
+    },
+  ],
+  social: [
+
+    {
+      name: "facebook",
+      username: "kaijun",
+      url: "https://www.facebook.com/demonbane.cn"
+    },
+    {
+      name: "weibo",
+      username: "Demonbane",
+      url: "http://weibo.com/1656320160/profile?topnav=1&wvr=6"
+    },
+  ],
+  experiences: [
+    {
+      company: "TU Dresden",
+      period: "Nov 2011 - Present",
+      title: "Lead Design & Frontend Developer",
+      desc: "担任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草任职位窝草"
+    },
+    {
+      company: "TU Dresden",
+      period: "Nov 2011 - Present",
+      title: "Lead Design & Frontend Developer",
+      desc: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    }
+  ],
+  projects: [
+    {
+      title: "Lead Design & Frontend Developer",
+      desc: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+      url: "http://example.com"
+    },
+    {
+      title: "Lead Design & Frontend Developer",
+      desc: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+      url: "http://example.com"
+    }
+  ],
+  educations: [
+    {
+      major: "网络工程",
+      degree: "学士",
+      college: "西安电子科技大学",
+      period: "Nov 2011 - Nov 2014",
+    },
+    {
+      major: "Informatik",
+      degree: "Master",
+      college: "TU Dresden",
+      period: "Nov 2011 - Nov 2014",
+    }
+  ],
+  skills:[
+    {
+      name: "JavaScript",
+      score: 10
+    },
+    {
+      name: "HTML",
+      score: 6
+    },
+    {
+      name: "NodeJS",
+      score: 9
+    },
+    {
+      name: "CSS",
+      score: 8
+    },
+    {
+      name: "Android",
+      score: 2
+    },
+  ]
+}
